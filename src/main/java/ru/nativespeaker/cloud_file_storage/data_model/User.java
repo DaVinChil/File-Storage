fb7246c9d12a73b69d9e248b6a47ff9d310d86c3
DaVinCi <fox.winner9@gmail.com>
2023-08-20 14:36:02 +0300

    add token to user entity

--- src/main/java/ru/nativespeaker/cloud_file_storage/data_model/User.java
+++ src/main/java/ru/nativespeaker/cloud_file_storage/data_model/User.java
@@ -2,10 +2,17 @@ package ru.nativespeaker.cloud_file_storage.data_model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
+@Data
 @Table(schema = "file_storage", name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
@@ -18,4 +25,42 @@ public class User {
     @Column(name = "password")
     @NotNull
     private String password;
+
+    @OneToOne(mappedBy = "user")
+    private Token tokens;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
