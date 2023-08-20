d8ab9a40d84ef5f543b9e6e4182ed13e8e97000f
DaVinCi <fox.winner9@gmail.com>
2023-08-20 15:08:10 +0300

    add token repository

---
+++ src/main/java/ru/nativespeaker/cloud_file_storage/repository/TokenRepository.java
@@ -0,0 +1,10 @@
+package ru.nativespeaker.cloud_file_storage.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.nativespeaker.cloud_file_storage.data_model.Token;
+
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+    Optional<Token> findByUuid(String uuid);
+}
