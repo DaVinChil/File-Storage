b9dd731223ace9acf28ed3b95c07521449d654bd
DaVinCi <fox.winner9@gmail.com>
2023-08-20 16:55:17 +0300

    add token generator

---
+++ src/main/java/ru/nativespeaker/cloud_file_storage/service/auth/TokenGenerator.java
@@ -0,0 +1,30 @@
+package ru.nativespeaker.cloud_file_storage.service.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.nativespeaker.cloud_file_storage.data_model.Token;
+import ru.nativespeaker.cloud_file_storage.data_model.User;
+import ru.nativespeaker.cloud_file_storage.repository.TokenRepository;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class TokenGenerator {
+    private final TokenRepository tokenRepository;
+
+    private String getUniqueUuid() {
+        String uuid;
+        do {
+            uuid = UUID.randomUUID().toString();
+        } while (tokenRepository.existsByUuid(uuid));
+        return uuid;
+    }
+
+    public Token getUniqueToken(User user){
+        Token token = Token.builder().uuid(getUniqueUuid()).user(user).expirationDate(LocalDateTime.now().plusHours(12)).build();
+        tokenRepository.save(token);
+        return token;
+    }
+}
