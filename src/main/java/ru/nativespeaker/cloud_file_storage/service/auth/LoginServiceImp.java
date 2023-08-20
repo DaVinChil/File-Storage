ccb0840baa5c508c2cf46c1d020c0b169a2f3a9f
DaVinCi <fox.winner9@gmail.com>
2023-08-20 19:07:52 +0300

    fix login in sevice

--- src/main/java/ru/nativespeaker/cloud_file_storage/service/auth/LoginServiceImp.java
+++ src/main/java/ru/nativespeaker/cloud_file_storage/service/auth/LoginServiceImp.java
@@ -15,23 +15,24 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class LoginServiceImp implements LoginService{
+public class LoginServiceImp implements LoginService {
     private final TokenGenerator tokenGenerator;
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
+
     @Override
     public Optional<String> login(AuthorizationRequest request) {
+        Optional<User> user = userRepository.findByEmail(request.getLogin());
+        if (user.isEmpty()) {
+            return Optional.empty();
+        }
+
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.getLogin(),
                         request.getPassword()
                 )
         );
-
-        Optional<User> user = userRepository.findByEmail(request.getLogin());
-        if(user.isEmpty()){
-            return Optional.empty();
-        }
 
         Token token = tokenGenerator.getUniqueToken(user.get());
 
