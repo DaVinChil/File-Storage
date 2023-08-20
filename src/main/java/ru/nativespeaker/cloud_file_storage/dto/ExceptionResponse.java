6aec571897c03308fecfdcb4b19a160991e8c4a4
DaVinCi <fox.winner9@gmail.com>
2023-08-20 17:11:27 +0300

    add exception dto

---
+++ src/main/java/ru/nativespeaker/cloud_file_storage/dto/ExceptionResponse.java
@@ -0,0 +1,9 @@
+package ru.nativespeaker.cloud_file_storage.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ExceptionResponse {
+    private String message;
+    private Long id;
+}
