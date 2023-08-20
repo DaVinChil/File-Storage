420112e78b3c817ca299e8b82df72c86a48ce9e5
DaVinCi <fox.winner9@gmail.com>
2023-08-20 15:35:39 +0300

    add existsByUuid method in TokenRepository

--- src/main/java/ru/nativespeaker/cloud_file_storage/repository/TokenRepository.java
+++ src/main/java/ru/nativespeaker/cloud_file_storage/repository/TokenRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
     Optional<Token> findByUuid(String uuid);
+    boolean existsByUuid(String uuid);
 }
