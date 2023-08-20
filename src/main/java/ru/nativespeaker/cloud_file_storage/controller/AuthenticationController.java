e8b67cd34d4a426752e0fa0967dd7f9d9a2777c6
DaVinCi <fox.winner9@gmail.com>
2023-08-20 19:06:33 +0300

    add @requesetBody into method

--- src/main/java/ru/nativespeaker/cloud_file_storage/controller/AuthenticationController.java
+++ src/main/java/ru/nativespeaker/cloud_file_storage/controller/AuthenticationController.java
@@ -1,10 +1,11 @@
 package ru.nativespeaker.cloud_file_storage.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import ru.nativespeaker.cloud_file_storage.dto.AuthorizationRequest;
 import ru.nativespeaker.cloud_file_storage.dto.AuthorizationResponse;
 
 public interface AuthenticationController {
     @PostMapping("/login")
-    AuthorizationResponse authenticate(AuthorizationRequest request);
+    AuthorizationResponse authenticate(@RequestBody AuthorizationRequest request);
 }
