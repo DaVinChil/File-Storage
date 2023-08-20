07c78b1f3dc9caae38f709b5bb8f8c42c68d1d22
DaVinCi <fox.winner9@gmail.com>
2023-08-20 16:57:22 +0300

    add Authentication controllerj

---
+++ src/main/java/ru/nativespeaker/cloud_file_storage/controller/AuthenticationControllerImp.java
@@ -0,0 +1,17 @@
+package ru.nativespeaker.cloud_file_storage.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import ru.nativespeaker.cloud_file_storage.dto.AuthorizationRequest;
+import ru.nativespeaker.cloud_file_storage.dto.AuthorizationResponse;
+import ru.nativespeaker.cloud_file_storage.service.auth.AuthenticationService;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthenticationControllerImp implements AuthenticationController{
+    private final AuthenticationService authenticationService;
+    @Override
+    public AuthorizationResponse authenticate(AuthorizationRequest request) {
+        return new AuthorizationResponse(authenticationService.authenticate(request));
+    }
+}
