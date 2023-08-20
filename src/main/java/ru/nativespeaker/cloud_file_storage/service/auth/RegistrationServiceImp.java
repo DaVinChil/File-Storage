08e37bc5aa9aee1ae2ffc7d3df8e62b29892c469
DaVinCi <fox.winner9@gmail.com>
2023-08-20 16:56:25 +0300

    add registration service

---
+++ src/main/java/ru/nativespeaker/cloud_file_storage/service/auth/RegistrationServiceImp.java
@@ -0,0 +1,36 @@
+package ru.nativespeaker.cloud_file_storage.service.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.nativespeaker.cloud_file_storage.data_model.Token;
+import ru.nativespeaker.cloud_file_storage.data_model.User;
+import ru.nativespeaker.cloud_file_storage.dto.AuthorizationRequest;
+import ru.nativespeaker.cloud_file_storage.exception.UserAlreadyExistsException;
+import ru.nativespeaker.cloud_file_storage.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RegistrationServiceImp implements RegistrationService{
+    private final UserRepository userRepository;
+    private final TokenGenerator tokenGenerator;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Optional<String> register(AuthorizationRequest request) {
+        if(userRepository.existsByEmail(request.getLogin())){
+            throw new UserAlreadyExistsException("User with " + request.getLogin() + " login already exists");
+        }
+
+        User user = User.builder()
+                .email(request.getLogin())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+        Token token = tokenGenerator.getUniqueToken(user);
+
+        return token.getUuid().describeConstable();
+    }
+}
