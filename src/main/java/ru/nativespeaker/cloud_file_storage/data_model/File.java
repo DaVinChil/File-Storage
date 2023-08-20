67ca1ce92f9e163e0c64e8085c06cbf008440d2e
DaVinCi <fox.winner9@gmail.com>
2023-08-20 13:03:37 +0300

    create file entity

---
+++ src/main/java/ru/nativespeaker/cloud_file_storage/data_model/File.java
@@ -0,0 +1,29 @@
+package ru.nativespeaker.cloud_file_storage.data_model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "files")
+public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "file_id")
+    private long id;
+
+    @NotNull
+    @ManyToOne
+    private User user;
+
+    @NotNull
+    @Column(name = "hash")
+    private String hash;
+
+    @NotNull
+    @Column(name = "file_name")
+    private String fileName;
+
+    @NotNull
+    @Column(name = "content")
+    private byte[] content;
+}
