87b15e8e37c163434607392ca6a91fa1a5ddfb20
DaVinCi <fox.winner9@gmail.com>
2023-08-20 15:36:38 +0300

    add AuthTokenFilter

---
+++ src/main/java/ru/nativespeaker/cloud_file_storage/security/AuthTokenFilter.java
@@ -0,0 +1,57 @@
+package ru.nativespeaker.cloud_file_storage.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import ru.nativespeaker.cloud_file_storage.service.auth.AuthTokenService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class AuthTokenFilter extends OncePerRequestFilter {
+    private final AuthTokenService tokenService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if(request.getServletPath().contains("/login")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String authHeader = request.getHeader("auth-token");
+        final String token;
+        final String userEmail;
+        if(authHeader == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        token = authHeader.substring(7);
+        userEmail = tokenService.getUserEmail(token);
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+            boolean isTokenValid = tokenService.isExist(token);
+            if(isTokenValid){
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        null
+                );
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+    }
+}
