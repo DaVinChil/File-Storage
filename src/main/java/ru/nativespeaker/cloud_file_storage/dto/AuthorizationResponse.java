19d312e837eee9772aca301acff9662e23606bad
DaVinCi <fox.winner9@gmail.com>
2023-08-20 16:05:46 +0300

    add all args constructor t authorizationResponse

--- src/main/java/ru/nativespeaker/cloud_file_storage/dto/AuthorizationResponse.java
+++ src/main/java/ru/nativespeaker/cloud_file_storage/dto/AuthorizationResponse.java
@@ -1,9 +1,11 @@
 package ru.nativespeaker.cloud_file_storage.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class AuthorizationResponse {
     @JsonProperty("auth-token")
     private String authToken;
