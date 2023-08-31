520ae40a1285e4085875e4a84ebc1e72d5832b60
DaVinCi <fox.winner9@gmail.com>
2023-08-31 14:08:57 +0300

    no such file exception handelling

--- src/main/java/ru/nativespeaker/cloud_file_storage/handler/AllExceptionHandler.java
+++ src/main/java/ru/nativespeaker/cloud_file_storage/handler/AllExceptionHandler.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import ru.nativespeaker.cloud_file_storage.data_model.User;
 import ru.nativespeaker.cloud_file_storage.dto.ExceptionResponse;
 import ru.nativespeaker.cloud_file_storage.exception.InternalServerException;
+import ru.nativespeaker.cloud_file_storage.exception.NoSuchFileException;
 import ru.nativespeaker.cloud_file_storage.exception.UserAlreadyExistsException;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -17,13 +17,19 @@ public class AllExceptionHandler {
 
     @ExceptionHandler(InternalServerException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ExceptionResponse internalServerException(InternalServerException e){
+    public ExceptionResponse internalServerError(InternalServerException e){
         return new ExceptionResponse(e.getMessage(), atomicLong.getAndIncrement());
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ExceptionResponse userAlreadyExistsException(UserAlreadyExistsException e){
+    public ExceptionResponse userAlreadyExists(UserAlreadyExistsException e){
+        return new ExceptionResponse(e.getMessage(), atomicLong.getAndIncrement());
+    }
+
+    @ExceptionHandler(NoSuchFileException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse badFileName(NoSuchFileException e) {
         return new ExceptionResponse(e.getMessage(), atomicLong.getAndIncrement());
     }
 }
