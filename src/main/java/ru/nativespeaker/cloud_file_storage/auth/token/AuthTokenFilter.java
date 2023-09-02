d774cd7e284be64618cbe450f35ff3d5c33f8fd0
DaVinCi <fox.winner9@gmail.com>
2023-09-02 08:37:50 +0300

    fix filter and add token expiration validation

--- src/main/java/ru/nativespeaker/cloud_file_storage/auth/token/AuthTokenFilter.java
+++ src/main/java/ru/nativespeaker/cloud_file_storage/auth/token/AuthTokenFilter.java
@@ -4,7 +4,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,48 +12,53 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import ru.nativespeaker.cloud_file_storage.user.User;
+import ru.nativespeaker.cloud_file_storage.user.UserRepository;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
 public class AuthTokenFilter extends OncePerRequestFilter {
+    private final UserRepository userRepository;
     private final AuthTokenService tokenService;
     private final UserDetailsService userDetailsService;
 
-    public AuthTokenFilter(AuthTokenService tokenService, @Lazy UserDetailsService userDetailsService) {
+    public AuthTokenFilter(UserRepository userRepository, AuthTokenService tokenService, @Lazy UserDetailsService userDetailsService) {
+        this.userRepository = userRepository;
         this.tokenService = tokenService;
         this.userDetailsService = userDetailsService;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if(request.getServletPath().contains("/login")) {
+        if (request.getServletPath().contains("/login")) {
             filterChain.doFilter(request, response);
             return;
         }
 
         final String authHeader = request.getHeader("auth-token");
         final String token;
-        final String userEmail;
-        if(authHeader == null) {
+        if (authHeader == null) {
             filterChain.doFilter(request, response);
             return;
         }
 
         token = authHeader.substring(7);
-        userEmail = tokenService.getUserEmail(token);
-        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
-            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
-            boolean isTokenValid = tokenService.isExist(token);
-            if(isTokenValid){
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails,
-                        null,
-                        null
-                );
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
-            }
+        if(!tokenService.isValid(token)) {
+            filterChain.doFilter(request,response);
+            return;
         }
+        Optional<User> user = userRepository.findByToken_Uuid(token);
+        if (user.isPresent() && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.get().getUsername());
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    null
+            );
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+        filterChain.doFilter(request, response);
     }
 }
--- src/main/java/ru/nativespeaker/cloud_file_storage/auth/token/AuthTokenService.java
+++ src/main/java/ru/nativespeaker/cloud_file_storage/auth/token/AuthTokenService.java
@@ -6,17 +6,27 @@ import ru.nativespeaker.cloud_file_storage.user.User;
 import ru.nativespeaker.cloud_file_storage.exception.InternalServerException;
 import ru.nativespeaker.cloud_file_storage.user.UserRepository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class AuthTokenService {
     private final TokenRepository tokenRepository;
     private final UserRepository userRepository;
 
-    public boolean isExist(String token){
+    public boolean isExist(String token) {
         return tokenRepository.existsByUuid(token);
     }
 
-    public String getUserEmail(String token){
-        return tokenRepository.findByUuid(token).map(userRepository::findByToken).map(opt -> opt.orElseThrow(() -> new InternalServerException("Cant find user by token"))).map(User::getEmail).orElse(null);
+    public boolean isValid(String token) {
+        if(!isExist(token)) {
+            return false;
+        }
+        Token entityToken = tokenRepository.findByUuid(token).get();
+        if(entityToken.getExpirationDate().isBefore(LocalDateTime.now())) {
+            return false;
+        }
+        return true;
     }
 }
